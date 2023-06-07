5fdf4d9d39b0cc1217b2c4d4ea43f535858e0314
Павел <107019787+Pol888@users.noreply.github.com>
2023-06-07 09:03:02 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,40 +5,40 @@ import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) throws Exception {
         // 1 task
-        //new Ex2().printFloat();
+        new Ex2().printFloat();
         //--------------------------------------------
         // 2 task
-        //int[] intArray = new int[]{3, 4, 6, 7, 6, 5, 4, 3};
+        int[] intArray = new int[]{3, 4, 6, 7, 6, 5, 4, 3};
 //
-        //try {
-        //        int d = 0;
-        //        double catchedRes1 = intArray[8] / d;
-        //        System.out.println("catchedRes1 = " + catchedRes1);
-        //    } catch (IndexOutOfBoundsException e){
-        //        System.out.println("IndexOutOfBounds exception: " + e);
-        //    }
-        //catch (ArithmeticException  e) {
-        //        System.out.println("Catching exception: " + e);
-        //    } catch (Exception e){
-        //        System.out.println("...................................");
-        //}
+        try {
+                int d = 0;
+                double catchedRes1 = intArray[8] / d;
+                System.out.println("catchedRes1 = " + catchedRes1);
+            } catch (IndexOutOfBoundsException e){
+                System.out.println("IndexOutOfBounds exception: " + e);
+            }
+        catch (ArithmeticException  e) {
+                System.out.println("Catching exception: " + e);
+            } catch (Exception e){
+                System.out.println("...................................");
+        }
         //--------------------------------------------------------
-        //
-            //try {
-            //        int a = 90;
-            //        int b = 3;
-            //        System.out.println(a / b);
-            //        printSum(23, 234);
-            //        int[] abc = { 1, 2 };
-            //        abc[3] = 9;
-            //    } catch (NullPointerException ex) {
-            //        System.out.println("Указатель не может указывать на null!");
-            //    } catch (IndexOutOfBoundsException ex) {
-            //        System.out.println("Массив выходит за пределы своего размера!");
-            //    }
-            //catch (Throwable ex) {
-            //        System.out.println("Что-то пошло не так...");
-            //    }
+        
+            try {
+                    int a = 90;
+                    int b = 3;
+                    System.out.println(a / b);
+                    printSum(23, 234);
+                    int[] abc = { 1, 2 };
+                    abc[3] = 9;
+                } catch (NullPointerException ex) {
+                    System.out.println("Указатель не может указывать на null!");
+                } catch (IndexOutOfBoundsException ex) {
+                    System.out.println("Массив выходит за пределы своего размера!");
+               }
+            catch (Throwable ex) {
+                    System.out.println("Что-то пошло не так...");
+                }
 
         new Ex2().printString();
 
